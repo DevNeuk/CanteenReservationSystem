a6402e41ddb22f24ac4e00f89c6742076a6b1537
JOE FREEDA <joefreeda.30@gmail.com>
2020-12-06 13:10:53 +0000

    insertorder

--- CanteenReservation/database.py
+++ CanteenReservation/database.py
@@ -37,25 +37,7 @@ class DataBase(Resource):
      cur.execute("""INSERT INTO db_registration (name,password,email,phone_no,role,unique_id)
                     VALUES (%s,%s,%s,%s,%s,%s)""",("Admin","admin@123","admin@sereneneuk.ac.uk","0227894556","admin","1"))
 
-    if(self.table_exists(connection,"db_booking")):
-   
-      cur.execute('''CREATE TABLE db_booking
-      (ID SERIAL PRIMARY KEY,
-      booking_status     TEXT     NOT NULL,
-      total_price     TEXT     NOT NULL,
-      booking_id     TEXT     NOT NULL,  
-      total_count     TEXT     NOT NULL,
-      unique_id      TEXT     NOT NULL);''')
-
-
-    if(self.table_exists(connection,"db_bookingitems")):
-   
-      cur.execute('''CREATE TABLE db_bookingitems
-      (ID SERIAL PRIMARY KEY,
-      item_price          TEXT     NOT NULL,
-      item_count       TEXT     NOT NULL,
-      item_name           TEXT     NOT NULL,
-      booking_id      TEXT     NOT NULL);''')
+    
 
 
     if(self.table_exists(connection,"db_menu")):
@@ -118,12 +100,33 @@ class DataBase(Resource):
      cur.execute("""INSERT INTO db_menu (item_name,item_description,item_price,item_calories)
                     VALUES (%s,%s,%s,%s)""",
                     ("Cappuccino","A double shot of Arabica bean espresso with steamed organic semi-skimmed milk, creating the perfect frothy texture. Totally delicious and topped with a chocolatey dusting.","1.90","406 kJ | 97 kcal"))
+
+
+    if(self.table_exists(connection,"db_booking")):
+   
+      cur.execute('''CREATE TABLE db_booking
+      (ID SERIAL PRIMARY KEY,
+      booking_status     TEXT     NOT NULL,
+      total_price     TEXT     NOT NULL,
+      booking_id     TEXT     NOT NULL,  
+      total_count     TEXT     NOT NULL,
+      unique_id      TEXT     NOT NULL);''')
+
+
+    if(self.table_exists(connection,"db_bookingitems")):
+   
+      cur.execute('''CREATE TABLE db_bookingitems
+      (ID SERIAL PRIMARY KEY,
+      item_price          TEXT     NOT NULL,
+      item_count       TEXT     NOT NULL,
+      item_name           TEXT     NOT NULL,
+      booking_id      TEXT     NOT NULL);''')
    
 
 
-     connection.commit()
-     cur.close()
-     connection.close()
+    connection.commit()
+    cur.close()
+    connection.close()
 
   except (Exception, psycopg2.Error) as error :
      print ("Error while connecting to PostgreSQL", error)
@@ -246,27 +249,27 @@ class DataBase(Resource):
      conn.close()
      return insertvalue[0]
 
- def bookorder(self,args) :
+ def bookorder(self,json_data) :
 
         bookingId = self.generaterbookingId()
-        jsonobj = json.loads(args['items'])                                 
 
-        for x in jsonobj['items']:
+        for x in json_data['items']:
             itemname = x['item_name']
             itemprice = x['item_price']
             item_count = x['item_count']
-                                        
             query2 = """INSERT INTO db_bookingitems (item_price,item_count,item_name,booking_id)
                     VALUES (%s,%s,%s,%s) RETURNING booking_id"""
-            insertbookingitemsdata = (item_price,item_count,item_name,booking_id)
+            insertbookingitemsdata = (itemprice,item_count,itemname,bookingId)
             self.insertQuery(query2,insertbookingitemsdata)
-        
-        query1 = """INSERT INTO db_booking (booking_status,total_price,booking_id,unique_id,total_count)
+      
+
+        query1 = """INSERT INTO db_booking (booking_status,total_price,booking_id,total_count,unique_id)
                     VALUES (%s,%s,%s,%s,%s) RETURNING booking_id"""
-        insertbookingdata = (args['unique_id'],
-                      args['booking_status'],
-                      bookingId,args['total_price'],args[total_count])
+        insertbookingdata = (
+                      json_data['booking_status'],
+                      json_data['total_price'],bookingId,json_data['total_count'],json_data['unique_id'])
         self.insertQuery(query1,insertbookingdata)
+        status = {"status": "1", "id": bookingId}
         return jsonify(status)
             
  
--- CanteenReservation/resource/booking.py
+++ CanteenReservation/resource/booking.py
@@ -1,21 +1,24 @@
 from flask_restful import Resource,reqparse
 from flask import request
 from CanteenReservation.database import DataBase
+import json
 
 class Booking(Resource):
    
   
     def insertOrderDetails(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('unique_id', type=str)
-        parser.add_argument('items', type=dict)
-        parser.add_argument('total_count', type=str)        
-        parser.add_argument('booking_status', type=str)
-        parser.add_argument('total_price', type=str)
+       # parser = reqparse.RequestParser()
+        #parser.add_argument('unique_id', type=str)
+       # parser.add_argument('items', type = list, location='json')
+       # parser.add_argument('total_count', type=str)         
+       # parser.add_argument('booking_status', type=str)
+        json_data = request.get_json()
+       # readable_json = json.dumps(json_data)
+       # parser.add_argument('total_price', type=str)
                
-        args = parser.parse_args()
+       # args = parser.parse_args()
         db = DataBase()
-        return db.bookorder(args)
+        return db.bookorder(json_data)
 
     def getCustomerInfo(self):
         parser = reqparse.RequestParser()
